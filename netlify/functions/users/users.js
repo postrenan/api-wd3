ac81a2465c55fb160a1aa84ead5b0f1b0551b0f3
Bick Renan <renanbickdev@gmail.com>
2024-06-17 23:31:14 -0300

    CORS

--- netlify/functions/users/users.js
+++ netlify/functions/users/users.js
@@ -1,16 +1,38 @@
 exports.handler = async (event, context) => {
-  if (event.httpMethod === 'GET') {
+  const headers = {
+    'Access-Control-Allow-Origin': '*', // Permite acesso de qualquer domínio
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+  };
 
+  if (event.httpMethod === 'OPTIONS') {
+    // Retorna os cabeçalhos para requisições de preflight do CORS
     return {
       statusCode: 200,
+      headers,
+      body: ''
+    };
+  }
+
+  if (event.httpMethod === 'GET') {
+    return {
+      statusCode: 200,
+      headers,
       body: JSON.stringify('ola mundo')
     };
   } else if (event.httpMethod === 'POST') {
-    // Lógica para adicionar um novo usuário
     const newUser = JSON.parse(event.body);
     return {
       statusCode: 201,
+      headers,
       body: `Usuário ${newUser.name} criado com sucesso!`
     };
   }
+
+  // Se o método HTTP não for nem GET nem POST, retorna um erro 405 Method Not Allowed
+  return {
+    statusCode: 405,
+    headers,
+    body: JSON.stringify({ error: "Método não permitido" })
+  };
 };
